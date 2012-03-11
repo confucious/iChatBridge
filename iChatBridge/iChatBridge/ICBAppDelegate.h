063b9057278e21b0a0c8b2c508e0d6b8bc1e617b
Jerry Hsu <jerry@gamechanger.io>
2012-03-11 19:17:58 -0400

    Empty project

---
+++ iChatBridge/iChatBridge/ICBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ICBAppDelegate.h
+//  iChatBridge
+//
+//  Created by Jerry Hsu on 3/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ICBAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ iChatBridge/iChatBridge/ICBAppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  ICBAppDelegate.m
+//  iChatBridge
+//
+//  Created by Jerry Hsu on 3/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ICBAppDelegate.h"
+
+@implementation ICBAppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
